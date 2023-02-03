3d55307eab47b3821e0a5bfe66988d26156a7be8
florenciavillar <FV218320@fi365.ort.edu.uy>
2023-02-02 21:22:07 -0300

    login ui

--- front-taller/src/components/App/App.js
+++ front-taller/src/components/App/App.js
@@ -2,13 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Login from '../login/Login';
 import Dashboard from '../dashboard/Dashboard';
+import { ChakraProvider } from '@chakra-ui/react'
+
 
 function App() {
   return (
-    <div className="App">
+    <ChakraProvider>
       <Login/>
-      <Dashboard/>
-    </div>
+    </ChakraProvider>
   );
 }
 
--- front-taller/src/components/login/Login.jsx
+++ front-taller/src/components/login/Login.jsx
@@ -1,15 +1,23 @@
 import "./Login.css"
+import { Card, CardHeader, CardBody, CardFooter, Stack, Box, Text, Heading, Input, Button, Link } from '@chakra-ui/react'
+
 const Login = () => {
-return <div>
-    <h2>Hola soy el login</h2>
-    <hr/>
-    <label>User</label>
-    <input type="text" placeholder="username"/>
-    <br/>
-    <label>Password</label>
-    <input type="text" placeholder="contrasena"/>
-    <br/>
-    <button type="submit">Submit</button>
+return <div class="login">
+    <Card class='card'>
+        <CardHeader class="heading"><Heading>Log in</Heading></CardHeader>
+        <CardBody>
+            <Stack>
+                <br/>
+                <Input type="text" placeholder="username"/>
+                <br/>
+                <Input type="text" placeholder="password"/>
+                <br/>
+                <Text>Not Registered? <Link color='teal.500'>Register</Link></Text>
+                <br/>
+                <Button type="submit" >Submit</Button>
+            </Stack>
+        </CardBody>
+    </Card>
 </div>
 }
 
