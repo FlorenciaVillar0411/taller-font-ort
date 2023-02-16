64063254200878b0e4d084121c47275a39e2b142
Florencia Villar <florencia.villar@adtran.com>
2023-02-16 12:28:20 -0300

    movis

--- front-taller/src/components/dashboard/Dashboard.jsx
+++ front-taller/src/components/dashboard/Dashboard.jsx
@@ -1,27 +1,26 @@
-import { Button } from "@chakra-ui/react";
-import AddForm from "./addForm/addForm";
-import { useDispatch } from 'react-redux'
-import { setLogoutUser } from "../../app/slices/userSlice";
+import { Button } from '@chakra-ui/react';
+import AddForm from './addForm/addForm';
+import Movimientos from './movimientos/Movimientos';
+import { useDispatch } from 'react-redux';
+import { setLogoutUser } from '../../app/slices/userSlice';
 //import { useEffect, useState } from "react";
 //import { getMovimientos } from "../../services/dwallet";
 //import MovimientosFilter from './filter/Filter
 
 const Dashboard = (props, user) => {
-
-
   //const [movimientos, setMovimientos] = useState([]) ESTE ES EL ORIGINAL
   //const [MovimientosFiltered, setFilteredMovimientos] = useState([]) ESTA ES LA LISTA FILTRADA
 
   //useEffect(() => {
 
   //getMovimientos(user.idUsuario, user.token).then(data => {
-        //setMovimientos(data)
-   // })
-    //.catch(e => console.error('Se ha producido un error'))
+  //setMovimientos(data)
+  // })
+  //.catch(e => console.error('Se ha producido un error'))
   //}, []);
 
   //const onFilterMovimientos = filter => {
-    /*if(filter == "gastos"){
+  /*if(filter == "gastos"){
       const gastos = movimientos.filter(gastoMov => !gastoMov.ingresos)
       setFilteredMovimientos(gastos)
     }else if(filter == "inversiones"){
@@ -35,11 +34,12 @@ const Dashboard = (props, user) => {
   const dispatch = useDispatch();
 
   const onLogOutClick = () => {
-    dispatch(setLogoutUser())
-  }
+    dispatch(setLogoutUser());
+  };
 
   return (
     <div>
+      <Movimientos></Movimientos>
       <AddForm></AddForm>
       <Button onClick={onLogOutClick}>LogOut</Button>
       {/*<GetMovimientos onFilter={onFilterMovimientos} />*/}
---
+++ front-taller/src/components/dashboard/movimientos/Movimientos.jsx
@@ -0,0 +1,10 @@
+import { Box, Heading } from '@chakra-ui/react';
+
+const Movimientos = (props) => {
+  return (
+    <Box>
+      <Heading>Movimientos</Heading>
+    </Box>
+  );
+};
+export default AddForm;
